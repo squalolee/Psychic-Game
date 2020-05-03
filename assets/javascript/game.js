688ce91990a91e01ce15d6fdab8bd8303f11226e
Solomon Lee <squalolee@gmail.com>
2020-05-03 18:36:04 -0400

    Added starting variables to javascirpt

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var lettersList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var chosenLetter = "";
+
+var wrongGuesses = [];
+
+var winsCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
