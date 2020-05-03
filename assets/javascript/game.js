cc56cada731917474b547e8f550aca2ad4940ee3
Solomon Lee <squalolee@gmail.com>
2020-05-03 18:57:45 -0400

    finished started game function, which will reset the game for each round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,3 +6,17 @@ var wrongGuesses = [];
 var winsCounter = 0;
 var lossCounter = 0;
 var numGuesses = 9;
+
+function startGame() {
+    numGuesses = 9;
+    chosenLetter = lettersList[Math.floor(math.random()*lettersList.length)];
+    
+    //testing
+    console.log(chosenLetter)
+    wrongGuesses = [];
+
+    document.getElementById("gLeft").innerHTML = numGuesses;
+    document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+}
