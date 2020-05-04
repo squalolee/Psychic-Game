984574ca30efbbe7ca14354ed7d41cd2133519e7
Solomon Lee <squalolee@gmail.com>
2020-05-03 22:04:16 -0400

    Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ var numGuesses = 9;
 
 function startGame() {
     numGuesses = 9;
-    chosenLetter = lettersList[Math.floor(math.random()*lettersList.length)];
+    chosenLetter = lettersList[Math.floor(Math.random() * lettersList.length)];
     
     //testing
     console.log(chosenLetter)
@@ -20,15 +20,58 @@ function startGame() {
 
 }
 
-function letterCheck(letter) {
-    var letterRight = false;
+// function letterCheck(letter) {
+   
 
-    if (chosenLetter === letter) {
-        winsCounter++;
-        numGuess--;
+//     if (chosenLetter === letter) {
+//         winsCounter++;
+//         numGuesses--;
+        
+//     }
+//     else {
+//         lossCounter++;
+//         numGuesses--;
+    
+//     }
+// }
+
+function psychicGame(letter) {
+    document.getElementById("gLeft").innerHTML = numGuesses;
+    
+    var continueCheck = true;
+
+    while (continueCheck = true) {
+        if (chosenLetter === letter) {
+            winsCounter++;
+            continueCheck =false;
+            alert("You win!");
+            document.getElementById("dWins").innerHTML = winsCounter;
+            
+        }
+        else if (numGuesses === 0) {
+            lossCounter++;
+            continueCheck =false;
+            alert("You lose!");
+            document.getElementById("dLosses").innerHTML = lossCounter;
+        }
+        else {
+            wrongGuesses.push(letter);
+            numGuesses--;
+            alert("Try Again!");
+            document.getElementById("gLeft").innerHTML = numGuesses;
+            document.getElementById("gHistory").innerHTML = wrongGuesses.join(" ");
+        
+        }
+    
     }
-    else {
-        lossCounter++;
-        numGuesses--;
-    }
+   
 }
+
+startGame();
+document.onkeyup = function(event) {
+
+    letterGuessed = String.fromCharCode(event.which).toLowerCase();
+  
+    psychicGame(letterGuessed);
+  };
+  
